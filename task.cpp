c1efd5fe09e039614269851ccbfd0b952f0aec99
elpidifor <elpidifor011@yandex.ru>
2017-09-29 11:12:21 +0300

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -9,42 +9,42 @@ int main() {
     std::cin >> z;
 
     //каждое из чисел X и Y нечетное;
-    if(!((x%4)&&(y%2))){
+    if (!((x%4) && (y%2))){
         std::cout << "TRUE" << std::endl;
     }else{
         std::cout << "FALSE" << std::endl;
     }
 
     //только одно из чисел X и Y меньше 20;
-    if(!((x<20)||(y<20))){
+    if (!((x<20) || (y<20))){
         std::cout << "TRUE" << std::endl;
     }else{
         std::cout << "FALSE" << std::endl;
     }
 
     //хотя бы одно из чисел X и Y равно нулю;
-    if((x==0)||(y==0)){
+    if ((x==0) || (y==0)){
         std::cout << "TRUE" << std::endl;
     }else{
         std::cout << "FALSE" << std::endl;
     }
 
     //каждое из чисел X, Y, Z отрицательное;
-    if((x<0)&&(y<0)&&(z<0)){
+    if ((x<0) && (y<0) && (z<0)){
         std::cout << "TRUE" << std::endl;
     }else{
         std::cout << "FALSE" << std::endl;
     }
 
     //только одно из чисел X, Y и Z кратно пяти;
-    if(!(!(x%5)|| !(y%5)|| !(z%5))){
+    if (!(!(x%5) || !(y%5) || !(z%5))){
         std::cout << "TRUE" << std::endl;
     }else{
         std::cout << "FALSE" << std::endl;
     }
 
     //хотя бы одно из чисел X, Y, Z больше 100
-    if((x>100)||(y>100)||(z>100)){
+    if ((x>100) || (y>100) || (z>100)){
         std::cout << "TRUE" << std::endl;
     }else{
         std::cout << "FALSE" << std::endl;
@@ -52,3 +52,4 @@ int main() {
 
 
     return 0;
+}
